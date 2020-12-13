164990cac40927aa88602cb64c96bdf21448b93f
VeganCzech <63636174+VeganCzech@users.noreply.github.com>
2020-12-14 00:20:40 +0100

    Upload to GIT after KIT (already uploaded)

---
+++ assets/assets/js/main.js
@@ -0,0 +1,20 @@
+/* Show menu*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/* Close menu */
+const navLink = document.querySelectorAll('.nav_link'),
+    navMenu = document.getElementById('nav-menu')
+function linkAction() {
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
